dc5c1054ad41d04df996947bf157c5d70e13252a
TOtoyama3 <hosi.mallon@gmail.com>
2026-08-26 01:16:07 +0900

    ver.4.0 AIの回答を音声で読み上げる(TTS)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import record
 import analysis_whisper
 import response_groq
+import speak_text
 
 COUNT_MAX = 10
 
@@ -14,11 +15,14 @@ def main():
         wav_data = record.record_audio()
 
         #録音データを分析
-
         analysis_text = analysis_whisper.transcribe_audio(wav_data)
 
+        #AIの回答を作成
         ai_text, history = response_groq.generate_response(analysis_text, history)
 
-        print(ai_text)
+        print(ai_text) #debug
+
+        #回答を読み上げる
+        speak_text.speak(ai_text)
 
 main()
