90999938391e8d3812ce8f8cbb40c73fbc46dfde
TOtoyama3 <hosi.mallon@gmail.com>
2026-08-27 23:39:41 +0900

    ver11.0.DiscordBotでwav音声を取得

--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,11 @@
 import discord
+from discord.ext import voice_recv
 import secret_manager
 
 TOKEN = secret_manager.api_manager("discord_token")
+
 VOICE_CHANNEL_NAME = secret_manager.name_manager("discord_server")
-# Botに必要な権限
+
 intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
@@ -15,15 +17,27 @@ async def on_ready():
 
     for guild in client.guilds:
         for channel in guild.voice_channels:
+
             if channel.name == VOICE_CHANNEL_NAME:
+
                 if guild.voice_client is not None:
-                    print("すでにボイスチャンネルへ接続しています")
+                    print("すでに接続しています")
                     return
+
                 print(f"接続開始: {channel.name}")
 
-                await channel.connect()
+                voice_client = await channel.connect(
+                    cls=voice_recv.VoiceRecvClient
+                )
 
-                print("接続完了")
+                print("ボイスチャンネルに接続しました")
+
+                # 音声受信開始
+                voice_client.listen(
+                    voice_recv.WaveSink("discord_audio.wav")
+                )
+
+                print("音声受信開始")
                 return
 
     print("指定したボイスチャンネルが見つかりません")
